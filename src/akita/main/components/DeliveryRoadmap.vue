160148b36ddc6c5e2f480050fffdd03e3642d140
Maxuta Pavel <MaxutaPavel@gmail.com>
2019-07-12 16:37:44 +0300

    change roadmap

--- src/akita/main/components/DeliveryRoadmap.vue
+++ src/akita/main/components/DeliveryRoadmap.vue
@@ -8,9 +8,12 @@
         <ul>
           <li v-for="item in roadmap">
             <div class="block" v-show="item.left" :style="{ 'margin-top': item.margin + 'px' }">
-              <p class="text bold right">{{item.year}}</p>
+              <div v-if="item.finished" class="text bold year blue">{{item.year}}</div>
+              <div v-else class="text bold year">{{item.year}}</div>
               <h4 class="text bold right">{{item.title}}</h4>
               <p class="text normal">{{item.text}}</p>
+              <p class="text normal text__description">{{item.description}}</p>
+              <img class="img" :src="item.img" :alt="item.alt">
             </div>
           </li>
         </ul>
@@ -19,9 +22,12 @@
         <ul>
           <li v-for="item in roadmap">
             <div class="block" v-if="!item.left" :style="{ 'margin-top': item.margin + 'px' }">
-              <p class="text bold">{{item.year}}</p>
+              <div v-if="item.finished" class="text bold year blue">{{item.year}}</div>
+              <div v-else class="text bold year">{{item.year}}</div>
               <h4 class="text bold">{{item.title}}</h4>
               <p class="text normal">{{item.text}}</p>
+              <p class="text normal text__description">{{item.description}}</p>
+              <img class="img" :src="item.img" :alt="item.alt">
             </div>
           </li>
         </ul>
@@ -40,82 +46,89 @@ export default {
         {
           left: true,
           year: "2017",
+          finished: true,
           title: "Distributed Data Market",
-          text: `AKITA and 60 companies like Bosch, Telekom, EY participate in a closed beta test of a decentralized “ebay” for sensor data directly bought, 
-                sold and documented on a global distributed platform by IOTA. This project drove IOTA’s token price from less than 1$ to 5$ in less than a month..`,
-          img: "#",
-          alt: "lorem",
+          text: "AKITA and 60 companies like Bosch, Telekom, EY participate in a closed beta test of a decentralized “ebay” for sensor data directly bought, sold and documented on a global distributed platform by IOTA.",
+          description: "This project drove IOTA’s token price from less than 1$ to 5$ in less than a month..",
+          img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdmtlOau9rrVih-xbzQg-LLpauw8JoRCt9itEvxhD3WYV_A3Vv",
+          alt: "network",
           margin: 0
         },
         {
           left: false,
           year: "2018",
+          finished: true,
           title: "Industry 4.0 Audit Trail",
-          text: `Fujitsu presented the worlds first live Production Audit Trail showcase at the HMI 2018 with components and robots. 
-                The complete blockchain software integration was delivered by AKITA as official supplier. 
-                Dominated Social Media #HMI with more 4.5 million organic impressions in 3 days on twitter alone.`,
-          img: "#",
-          alt: "lorem",
-          margin: 250
+          text: "Fujitsu presented the worlds first live Production Audit Trail showcase at the HMI 2018 with components and robots. The complete blockchain software integration was delivered by AKITA as official supplier.",
+          description: "Dominated Social Media #HMI with more 4.5 million organic impressions in 3 days on twitter alone.",
+          img: "http://www.imgworlds.com/wp-content/uploads/2015/12/18-CONTACTUS-HEADER.jpg",
+          alt: "gumball",
+          margin: 350
         },
         {
           left: true,
           year: "2018",
+          finished: true,
           title: "ID & Access Solution",
-          text: `A global personal ID combined with cutting edge biometric tech was build in cooperation with the e-shelter innovation lab. 
-                Registration, Authentication and tamper proof access log files. Nominated for the German Data Center Innovation Award.`,
-          img: "#",
-          alt: "lorem",
-          margin: 350
+          text: "A global personal ID combined with cutting edge biometric tech was build in cooperation with the e-shelter innovation lab. Registration, Authentication and tamper proof access log files. ",
+          description: "Nominated for the German Data Center Innovation Award.",
+          img: "http://www.imgworlds.com/wp-content/uploads/2015/12/18-CONTACTUS-HEADER.jpg",
+          alt: "gumball",
+          margin: 150
         },
         {
           left: false,
           year: "2019",
+          finished: true,
           title: "Automated M2M payment",
-          text: `Modular machine-to-machine payment prototypes presented live on stage. The car pays a micro transaction based on the real time parking duration,
-                triggering the parking space to an automated tax payment. AKITA was invited with 19 other companies out of 700 applications to the Dubai Future Blockchain Summit.`,
-          img: "#",
-          alt: "lorem",
+          text: "Modular machine-to-machine payment prototypes presented live on stage. The car pays a micro transaction based on the real time parking duration, triggering the parking space to an automated tax payment.",
+          description: "AKITA was invited with 19 other companies out of 700 applications to the Dubai Future Blockchain Summit.",
+          img: "http://www.imgworlds.com/wp-content/uploads/2015/12/18-CONTACTUS-HEADER.jpg",
+          alt: "gumball",
           margin: 350
         },
         {
           left: true,
           year: "2019",
+          finished: true,
           title: "Virtual Blockchain Lab",
-          text: `The Virtual Lab demonstrates blockchain based live Use Cases in a fast and easy to use test environment. The perfect opportunity for corporates to to gain 
-                valuable experience fast and without any risks. Our corporate partner network signed on the first clients and it is growing fast now.`,
-          img: "#",
-          alt: "lorem",
+          text: "The Virtual Lab demonstrates blockchain based live Use Cases in a fast and easy to use test environment. The perfect opportunity for corporates to to gain valuable experience fast and without any risks.",
+          description: "Our corporate partner network signed on the first clients and it is growing fast now.",
+          img: "http://www.imgworlds.com/wp-content/uploads/2015/12/18-CONTACTUS-HEADER.jpg",
+          alt: "gumball",
           margin: 350
         },
         {
           left: false,
           year: "2019",
+          finished: false,
           title: "Physical Blockchain Lab ",
-          text: `We are pioneers in hardware integration to blockchain based software and services. Our strength linked to the modular design of the virtual lab will allow
-                seamless transfer to physical applications. Established collaboration with universities, signed NDAs with corporates from automotive, OEM and energy sectors.`,
-          img: "#",
-          alt: "lorem",
+          text: "We are pioneers in hardware integration to blockchain based software and services. Our strength linked to the modular design of the virtual lab will allow seamless transfer to physical applications.",
+          description: "Established collaboration with universities, signed NDAs with corporates from automotive, OEM and energy sectors.",
+          img: "http://www.imgworlds.com/wp-content/uploads/2015/12/18-CONTACTUS-HEADER.jpg",
+          alt: "gumball",
           margin: 350
         },
         {
           left: true,
           year: "2020",
+          finished: false,
           title: "Machine Bank",
-          text: `The mainstream breakthrough will be driven by previous projects like ours and real world productive applications that deliver on the promises of blockchain: shared, global,
-                fast, cost efficient and more secure. Fully autonomous driving cars, buying and offering services to other machines and turning your car into an asset will be game changer.`,
-          img: "#",
-          alt: "lorem",
+          text: "The mainstream breakthrough will be driven by previous projects like ours and real world productive applications that deliver on the promises of blockchain: shared, global, fast, cost efficient and more secure.",
+          description: "Fully autonomous driving cars, buying and offering services to other machines and turning your car into an asset will be game changer.",
+          img: "http://www.imgworlds.com/wp-content/uploads/2015/12/18-CONTACTUS-HEADER.jpg",
+          alt: "gumball",
           margin: 350
         },
         {
           left: false,
           year: "2021",
+          finished: false,
           title: "Machine Economy",
-          text: `Our modular service architecture enables us to extend functions,, services, network, infrastructures in all directions. 
-                AKITA’s first mover initiative will provide partners, investors and clients a unique position in the new markets.`,
-          img: "#",
-          alt: "lorem",
+          text: "Our modular service architecture enables us to extend functions,, services, network, infrastructures in all directions.",
+          description: "AKITA’s first mover initiative will provide partners, investors and clients a unique position in the new markets.",
+          img: "http://www.imgworlds.com/wp-content/uploads/2015/12/18-CONTACTUS-HEADER.jpg",
+          alt: "gumball",
           margin: 350
         }
       ]
@@ -131,21 +144,51 @@ export default {
 
   .roadmap {
     display: flex;
+    justify-content: center;
     margin: 30px 50px;
     text-align: justify;
   }
 
   .roadmap__left {
-    border-right: 2px dotted #BFBBF7;
+    border-right: 2px dotted #00B0F0;
+    width: 50%;
     padding: 50px;
   }
 
   .roadmap__right {
+    width: 50%;
     padding: 50px;
   }
 
   h4 {
     margin: 5px 0 20px;
+  }
+
+  .text__description {
+    margin-top: 20px;
+    color: #00B0F0;
+  }
+
+  .year {
+    position: absolute;
+    left: calc(50% - 30px);
+    width: 60px;
+    height: 60px;
+    text-align: center;
+    padding-top: 13px;
+    background-color: #fff;
+    border-radius: 50%;
+    border: 3px solid #00B0F0;
+    color: #00B0F0;
+  }
+
+  .blue {
+    background-color: #00B0F0;
+    color: #fff;
+  }
+
+  .img {
+    width: 100%;
   }
 
   @media(max-width: 1000px) {
@@ -160,6 +203,10 @@ export default {
     .roadmap__right {
       padding-right: 0;
     }
+
+    .year {
+      padding-top: 17px;
+    }
   }
 
   @media(max-width: 600px) {
@@ -170,6 +217,7 @@ export default {
     .roadmap__left {
       border: none;
       padding: 50px 0;
+      width: 100%;
     }
 
     .roadmap__right {
@@ -179,6 +227,11 @@ export default {
     .block {
       margin-top: 100px !important;
       display: block !important;
+    }
+
+    .year {
+      position: relative;
+      left: 75%;
     }
   }
 </style>
