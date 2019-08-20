2d7fbb83859cea0cc23c4ab7794e753b3de5caeb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-20 10:16:58 +0000

    Deploy machineeconomy/machines.akitablock.io to github.com/machineeconomy/machines.akitablock.io.git:gh-pages

---
+++ precache-manifest.61bc8e53279c59c970ab0074eb90730e.js
@@ -0,0 +1,242 @@
+self.__precacheManifest = [
+  {
+    "revision": "95af165d054e048718c3",
+    "url": "/machines.akitablock.io/css/app.05e60bb9.css"
+  },
+  {
+    "revision": "95af165d054e048718c3",
+    "url": "/machines.akitablock.io/js/app.7bd1dfe6.js"
+  },
+  {
+    "revision": "121fdcb1c8766da71388",
+    "url": "/machines.akitablock.io/js/chunk-vendors.97497965.js"
+  },
+  {
+    "revision": "dc1534e617190433054c4fd9295b3175",
+    "url": "/machines.akitablock.io/img/network-b.dc1534e6.png"
+  },
+  {
+    "revision": "23e8732567ff54fa93e6f4f8696f05a6",
+    "url": "/machines.akitablock.io/img/machineChain.23e87325.png"
+  },
+  {
+    "revision": "4a71f1a7ca0cbc39c9509ef2a12a66d2",
+    "url": "/machines.akitablock.io/img/machines.4a71f1a7.png"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/machines.akitablock.io/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/machines.akitablock.io/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/machines.akitablock.io/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/machines.akitablock.io/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/machines.akitablock.io/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "b1b6b3c0b5ae151c86c499788f89bf28",
+    "url": "/machines.akitablock.io/img/iota.b1b6b3c0.png"
+  },
+  {
+    "revision": "4203538821aabe69fce216bc67acace9",
+    "url": "/machines.akitablock.io/img/linkedin.42035388.png"
+  },
+  {
+    "revision": "b43a452a2e01aa6011c0b1ccff0a069e",
+    "url": "/machines.akitablock.io/img/twitter.b43a452a.png"
+  },
+  {
+    "revision": "6881954a33722fcf4b10aaee1eb34f51",
+    "url": "/machines.akitablock.io/img/oldMachine.6881954a.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/machines.akitablock.io/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/machines.akitablock.io/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/machines.akitablock.io/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/machines.akitablock.io/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/machines.akitablock.io/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "8da6e9c466e2f3dec2db3fa5e1065299",
+    "url": "/machines.akitablock.io/img/sedan.8da6e9c4.png"
+  },
+  {
+    "revision": "5014e0434b63761efa09a8897044ae85",
+    "url": "/machines.akitablock.io/img/github.5014e043.png"
+  },
+  {
+    "revision": "09ec919b1fc27e1babd1f923cbf0d8cb",
+    "url": "/machines.akitablock.io/img/dataMarket.09ec919b.png"
+  },
+  {
+    "revision": "1d734b6f0891de288e6e81ffc8df15f4",
+    "url": "/machines.akitablock.io/index.html"
+  },
+  {
+    "revision": "d6f7a1eac888dbde72c7dde0a56ca589",
+    "url": "/machines.akitablock.io/img/akita_logo_white.png"
+  },
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/machines.akitablock.io/img/brand/blue.png"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/machines.akitablock.io/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/machines.akitablock.io/img/brand/favicon.png"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/machines.akitablock.io/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "6fafe4baca9d50d61a898c84ade7afa3",
+    "url": "/machines.akitablock.io/img/brand/white.png"
+  },
+  {
+    "revision": "44df3cb2cab97ad098795c3188d4a98d",
+    "url": "/machines.akitablock.io/img/chain.png"
+  },
+  {
+    "revision": "3ea1c182203a5012f92a8466a7b4a4f2",
+    "url": "/machines.akitablock.io/img/AKITA M2M Supply Chain with machines based on DLT.png"
+  },
+  {
+    "revision": "22315789f67cc1a1e30e10a6fffbd9b3",
+    "url": "/machines.akitablock.io/img/FactoryItems-min.png"
+  },
+  {
+    "revision": "4303f31d6aacd43948cd3629fbacc297",
+    "url": "/machines.akitablock.io/img/FactoryRobot.png"
+  },
+  {
+    "revision": "44c14f3511783af52b0d0c43832c8572",
+    "url": "/machines.akitablock.io/img/iota_logo_blue.png"
+  },
+  {
+    "revision": "7195111502e92f4f5a0db1426a54cda6",
+    "url": "/machines.akitablock.io/img/WindTurbine.png"
+  },
+  {
+    "revision": "bfe0c2de782bd4941d617657fb444faa",
+    "url": "/machines.akitablock.io/img/chain-min.png"
+  },
+  {
+    "revision": "6aa842497903f628643bd369e652741c",
+    "url": "/machines.akitablock.io/img/chainTest.png"
+  },
+  {
+    "revision": "4f763b9735020cce1200e1b62a383a2c",
+    "url": "/machines.akitablock.io/img/SupplyChain.png"
+  },
+  {
+    "revision": "5d08c71cf05e40a61dbf38c9813c8ffe",
+    "url": "/machines.akitablock.io/img/chainBlue.png"
+  },
+  {
+    "revision": "f21f9275914ab344c0562b77c4943ed1",
+    "url": "/machines.akitablock.io/img/SolarCell.png"
+  },
+  {
+    "revision": "e844954c2bd84a20bd3622083df2c941",
+    "url": "/machines.akitablock.io/img/MachineChain.png"
+  },
+  {
+    "revision": "25efdaa64d966275183b514e2a30548c",
+    "url": "/machines.akitablock.io/img/FactoryItemsTest.png"
+  },
+  {
+    "revision": "504430a50df21a82346d16e2dbfbdaba",
+    "url": "/machines.akitablock.io/img/07.png"
+  },
+  {
+    "revision": "cd8701c6d11597e72578f805c8c26d79",
+    "url": "/machines.akitablock.io/img/Wind Turbine.G15.2k.png"
+  },
+  {
+    "revision": "0f28cc175dda13340f0c66c9d344cbdb",
+    "url": "/machines.akitablock.io/img/FactoryItems.png"
+  },
+  {
+    "revision": "2cef5bfd42d59b1913f60f9063fb66dc",
+    "url": "/machines.akitablock.io/img/AKITA - Virtual Lab - M2M Supply Chain - landing page .png"
+  },
+  {
+    "revision": "195f05b760ac8e890018ca20eb7d5ad8",
+    "url": "/machines.akitablock.io/img/imgonline-com-ua-Mirror-g8pwhdaqAqAokRBg.png"
+  },
+  {
+    "revision": "3069b2a9a0afd1e24164a6d3a564cd5c",
+    "url": "/machines.akitablock.io/img/Quadcopter.L14.2k.png"
+  },
+  {
+    "revision": "ffde970ef733204dfd1b945b0d98476a",
+    "url": "/machines.akitablock.io/img/chain1.png"
+  },
+  {
+    "revision": "264923a1694ba1d3b834eddc40997873",
+    "url": "/machines.akitablock.io/img/imgonline-com-ua-Mirror-g8pwhdaqAqAokRBg2.png"
+  },
+  {
+    "revision": "aac4a3e1d8d6df79f29a03f9adc2a9fc",
+    "url": "/machines.akitablock.io/img/imgonline-com-ua-Mirror-9EU8eJoeJR0.png"
+  },
+  {
+    "revision": "8dbb9fcb1d89f498b0d6760733848f5d",
+    "url": "/machines.akitablock.io/img/imgonline-com-ua-Mirror-g8pwhdaqAqAokRBg1.png"
+  },
+  {
+    "revision": "313206e16f5ea29b72a927846fde5e5f",
+    "url": "/machines.akitablock.io/img/Generic Hybrid Car.G03.2k.png"
+  },
+  {
+    "revision": "9726011658b5f57bda36f0ef700c00ce",
+    "url": "/machines.akitablock.io/img/Business Woman.G15.2k.png"
+  },
+  {
+    "revision": "1ba8a589a773069e84223658bcf4242a",
+    "url": "/machines.akitablock.io/img/Factory Robot.G07.2k (1).png"
+  },
+  {
+    "revision": "bfda58c5bf88cf6fbc29d17e025ccde2",
+    "url": "/machines.akitablock.io/img/sedan.png"
+  },
+  {
+    "revision": "bac4c24033ee454acc39cf75a8e633e4",
+    "url": "/machines.akitablock.io/img/imgonline-com-ua-Mirror-rzQHR4x70x8.png"
+  },
+  {
+    "revision": "112d13e796ea50e4f09deb579fe3f846",
+    "url": "/machines.akitablock.io/img/Industial Robot.G15.2k.png"
+  },
+  {
+    "revision": "df59d19c3f8be9ebdddbdabcd822816f",
+    "url": "/machines.akitablock.io/img/Solar Cell.H02.2k.png"
+  }
+];
